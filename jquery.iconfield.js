77081f457138d83dad42c463fbbdd36ac5916240
Yotam Ofek <yotam.ofek@gmail.com>
2012-10-10 13:52:20 +0300

    finished the code? documentation still needed

--- jquery.iconfield.js
+++ jquery.iconfield.js
@@ -1,6 +1,24 @@
 (function( $ ) {
 
 	var methods = {
+		isEventOnIcon : function( e ) {
+			var $this = $(this),
+				x = e.pageX - $this.position().left,
+				y = e.pageY - $this.position().top,
+				horizontalPadding = $this.iconfield( 'option', 'horizontal-padding' ),
+				verticalPadding = $this.iconfield( 'option', 'vertical-padding' ),
+				imageWidth = $this.iconfield( 'option', 'image-width' ),
+				imageHeight = $this.iconfield( 'option', 'image-height' );
+
+
+
+				return (
+					(x >= horizontalPadding && x <= horizontalPadding + imageWidth)
+					&&
+					(y >= verticalPadding && y <= verticalPadding + imageHeight)
+				);
+		},
+
 		refresh : function( ) {
 			return this.each( function() {
 
@@ -23,6 +41,7 @@
 					var current_height = Number($this.css( 'height' ).slice(0, -2))
 
 					// add padding if the field's current height is not high enough to include the image
+					// (+ vertical padding)
 					if( current_height < minimal_height ) {
 						var padding = (minimal_height - current_height) / 2;
 						$.extend( style_map, {
@@ -30,6 +49,13 @@
 							'padding-bottom'	:	padding,
 						} );
 					}
+
+					// add image dimensions to the data stored for this element
+					$.extend( data, {
+						'image-width'	:	image.width,
+						'image-height'	:	image.height
+					} );
+					$this.data( 'iconfield', data);
 					
 					// update the field's style
 					$this.css( style_map );
@@ -51,8 +77,74 @@
 				$this.data( 'iconfield', new Object() );
 				$this.iconfield( 'option', $.extend( {
 					'horizontal-padding'	:	2,
-					'vertical-padding'		:	4
+					'vertical-padding'		:	4,
+					'icon-cursor'			:	'auto'
 				}, options ) );
+
+				var events = [ 'click' ];
+
+				// create a custom event that is triggered when the icon is clicked
+				$this.on( 'click', function( e ) {
+					if($this.iconfield( 'isEventOnIcon', e ) ) {
+						$this.trigger( 'iconfield.click' );
+					}
+				} );
+
+				// bind to mousemove for simulating mouseenter and mouseleave events
+				$this.on( 'mousemove', function( e ) {
+					var data = $this.data( 'iconfield' ),
+						wasOnIcon = data['was-on-icon'] ? data['was-on-icon'] : false,
+						curOnIcon = $this.iconfield( 'isEventOnIcon', e );
+
+					// this is obvious
+					$this.trigger( 'iconfield.mousemove' );
+
+					// if the mouse is on the icon and wasn't before, or vice versa,
+					// trigger the relevant event and store the current position of
+					// the mouse
+					if( curOnIcon != wasOnIcon ) {
+						if( curOnIcon && !wasOnIcon ) {
+							$this.trigger( 'iconfield.mouseenter' );
+						}
+						else if( !curOnIcon && wasOnIcon ) {
+							$this.trigger( 'iconfield.mouseleave' );
+						}
+
+						data['was-on-icon'] = curOnIcon;
+						$this.data( 'iconfield', data );
+					}
+				} );
+
+				// bind to our simulated mouseenter event to change the cursor
+				// when the mouse is hovering over the icon
+				$this.on( 'iconfield.mouseenter', function( e ) {
+					var data = $this.data( 'iconfield' ),
+						curCursor = $this.css( 'cursor' );
+
+					// 'auto' means that no special cursor was specified by the user
+					if( data['icon-cursor'] != 'auto' &&
+						data['icon-cursor'] != curCursor) {
+						// save the cursor before it's changed
+						data['old-cursor'] = curCursor ? curCursor : 'auto';
+						$this.data( 'iconfield', data );
+
+						// change the cursor
+						$this.css( 'cursor', data['icon-cursor'] );
+					}
+				} );
+
+				// change the icon back to normal
+				$this.on( 'iconfield.mouseleave', function( e ) {
+					var data = $this.data( 'iconfield' );
+
+					// 'auto' means that no special cursor was specified by the user,
+					// meaning that there is nothing to change back to
+					if( data['icon-cursor'] != 'auto' ) {
+						// if no old cursor was stored (meaning that the mouse was never on the icon)
+						// change back to 'auto'
+						$this.css( 'cursor', data['old-cursor'] ? data['old-cursor'] : 'auto' );
+					}
+				} );
 
 				// refresh the styling
 				$this.iconfield( 'refresh' )
